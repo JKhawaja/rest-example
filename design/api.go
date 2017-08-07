8b768f929307471ebe9b1f50f5780ec8377b5105
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2017-08-07 16:16:11 -0500

    created goa design code as api specification

---
+++ design/api.go
@@ -0,0 +1,19 @@
+package api
+
+import (
+	// . "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = API("GitHub SSH Keys", func() {
+	Title("GitHub SSH Keys")
+	Description(`This API is for retrieving lists of public SSH keys based on github usernames.`)
+	Version("0.0.1")
+	Scheme("http")
+	Consumes("application/json", func() {
+		Package("github.com/goadesign/goa/encoding/json")
+	})
+	Produces("application/json", func() {
+		Package("github.com/goadesign/goa/encoding/json")
+	})
+})
---
+++ design/keys.go
@@ -0,0 +1,55 @@
+package api
+
+import (
+	. "github.com/goadesign/goa/design"
+	. "github.com/goadesign/goa/design/apidsl"
+)
+
+var _ = Resource("keys", func() {
+
+	BasePath("/keys")
+	Response(BadRequest)          // 400
+	Response(Unauthorized)        // 401
+	Response(NotFound)            // 404
+	Response(InternalServerError) // 500
+
+	Action("list", func() {
+		Description("Given a list of GitHub usernames, responds with list of public SSH keys for each User (associated to their GitHub account).")
+		Routing(POST(""))
+		Payload(ArrayOf(String))
+		Response(OK, CollectionOf(User))
+		Metadata("swagger:summary", "List public SSH Keys based on username (or set of usernames)")
+	})
+})
+
+// UserKey is the type that represents a Github user's public SSH Key
+var UserKey = Type("UserKey", func() {
+
+	Description("Type for a GitHub user's public SSH Key")
+
+	Attribute("id", Integer, "The ID of the public SSH key on GitHub.", func() {
+		Example(1234567890)
+	})
+	Attribute("key", String, "The public SSH key", func() {
+		Example("ssh-rsa ABC123 ...")
+	})
+})
+
+// User ...
+var User = MediaType("application/vnd.Keys+json", func() {
+
+	Description("Response Type for a GitHub User's list of public SSH Keys")
+
+	Attribute("username", String, "The username of the GitHub user.", func() {
+		Example("myname")
+	})
+	Attribute("keys", ArrayOf(UserKey), "The list of the Github user's public SSH keys.", func() {
+	})
+
+	Required("username", "keys")
+
+	View("default", func() {
+		Attribute("username")
+		Attribute("keys")
+	})
+})
