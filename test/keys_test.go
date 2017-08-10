6cd8aa3a32ad4785c236be4d80b7f3c48ebcef15
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2017-08-09 20:21:43 -0500

    basic unit test -- showing how to utilize generated client package easily

---
+++ test/keys_test.go
@@ -0,0 +1,36 @@
+package test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/JKhawaja/replicated/client"
+)
+
+// TestKeysListener is a basic unit test example using the generated goa client code
+func TestKeysLister(t *testing.T) {
+
+	tests := [][]string{
+		{"tom", "dave"},
+		{"john", "john", "john"},
+		{"1234", "5678", "9101112"},
+	}
+
+	for _, usernames := range tests {
+		keysClient := client.New(nil)
+
+		ctx := context.Background()
+		path := "localhost:8080" + client.ListKeysPath()
+		resp, err := keysClient.ListKeys(ctx, path, usernames)
+		if err != nil {
+			t.Fatalf("Query %s failed with: %+v", usernames, err)
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			t.Fatalf("Failed request: %s", usernames)
+		}
+
+		resp.Body.Close()
+	}
+}
