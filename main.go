c4d1860c2768decb842186cc62bf971eee7fde12
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2017-08-30 12:48:27 -0500

    self-deploy an application on kubernetes

--- main.go
+++ main.go
@@ -3,13 +3,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/JKhawaja/replicated/app"
 	. "github.com/JKhawaja/replicated/controllers"
 	"github.com/JKhawaja/replicated/services/github"
+	"github.com/JKhawaja/replicated/services/kubernetes"
+	"github.com/JKhawaja/replicated/services/s3"
+	. "github.com/JKhawaja/replicated/util"
 
 	"github.com/goadesign/goa"
 	"github.com/goadesign/goa/logging/logrus"
@@ -19,6 +24,49 @@ import (
 )
 
 func main() {
+	flag.Parse()
+
+	if kubernetes.Deploy {
+		log.Println("Building binary ...")
+		path, err := BuildBinary("rest-example")
+		if err != nil {
+			log.Fatalf("Could not build Go binary: %s", err.Error())
+		}
+
+		// TODO: specify your S3 bucket
+		// TODO: add aws keys and s3 bucket is 'kubernetes-test-1234'
+		s3Bucket := ""
+
+		client := s3.NewS3Client()
+		log.Println("Uploading binary ...")
+		url, err := client.Upload(s3Bucket, path)
+		if err != nil {
+			log.Fatalf("Could not upload binary to S3: %s", err.Error())
+		}
+
+		// TODO: specify where your KubeConfig file is
+		// $HOME/.kube/config check with `cat config` -- set up for minikube
+		kubeConfigPath := "$HOME/.kube/config"
+
+		k8, err := kubernetes.NewKubernetesClient(kubeConfigPath)
+		if err != nil {
+			log.Fatalf("Could not create a Kubernetes Client: %s", err.Error())
+		}
+
+		config := kubernetes.DeploymentConfig{
+			BURL: url,
+			Name: "rest-example",
+		}
+
+		log.Println("Deploying binary ...")
+		if err = k8.CreateDeployment(config); err != nil {
+			log.Fatalf("Could not create Kubernetes Deployment: %s", err.Error())
+		}
+
+		log.Printf("Binary %s successfully deployed to Kubernetes. Please verify with kubectl.", path)
+		os.Exit(0)
+	}
+
 	// Create service
 	service := goa.New("GitHub SSH Keys")
 
