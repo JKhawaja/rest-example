20397e53114690574c72bad5875f66810996f43e
Jeremy Khawaja <jeremy.khawaja@yahoo.com>
2017-08-30 12:47:19 -0500

    updated services interface with kubernetes client

--- services/services.go
+++ services/services.go
@@ -2,9 +2,11 @@ package services
 
 import (
 	"github.com/JKhawaja/replicated/services/github"
+	"github.com/JKhawaja/replicated/services/kubernetes"
 )
 
 // Services ...
 type Services interface {
 	github.Client
+	kubernetes.Client
 }
